13a58a22bb6dc5e876320ca1ebeef5c9440b7bd6
Joris Bayer <joris.bayer@protonmail.com>
2021-02-01 12:06:45 +0100

    Set highlighter color

--- src/render.rs
+++ src/render.rs
@@ -1,18 +1,17 @@
 use crate::linefile::{Layer, Pen, Stroke};
 use crate::notebook::Notebook;
 use crate::page::Page;
-use skia_safe::{Canvas, Document, Paint, Path};
+use skia_safe::{Canvas, Color, Document, Paint, PaintStyle, Path};
 use std::io::Write;
 
 // For size, see https://remarkablewiki.com/tech/filesystem
-const DOCUMENT_WIDTH: i32 = 1404;
-const DOCUMENT_HEIGHT: i32 = 1872;
-const STROKE_WIDTH: f32 = 4.0;
 
 pub fn render_notebook<Output: Write>(
     notebook: Notebook,
     output: &mut Output,
 ) -> Result<(), std::io::Error> {
+    // TODO: metadata
+
     let mut document = skia_safe::pdf::new_document(None);
 
     for page in notebook.pages {
@@ -29,10 +28,7 @@ pub fn render_notebook<Output: Write>(
 pub fn render_page(page: Page, document: Document) -> Result<Document, std::io::Error> {
     let mut document = document.begin_page((1404, 1874), None);
 
-    // Flip y coordinates:
-    // canvas.concat(Matrix::scale(1.0, -1.0))?;
-    // canvas.concat(Matrix::translate(0.0, -DOCUMENT_HEIGHT))?;
-
+    // TODO:
     // canvas.set_line_cap_style(CapStyle::Round)?;
     // canvas.set_line_join_style(JoinStyle::Round)?;
 
@@ -53,13 +49,14 @@ pub fn render_layer(layer: Layer, canvas: &mut Canvas) -> Result<(), std::io::Er
 
 pub fn render_stroke(stroke: Stroke, canvas: &mut Canvas) -> Result<(), std::io::Error> {
     // TODO: set fill color, pressure, etc.
-    // canvas.set_line_width(stroke.width)?;
-    // canvas.set_stroke_color(match stroke.pen {
-    //     Pen::Highlighter => Color::rgb(0, 255, 255),
-    //     _ => Color::gray(0),
-    // })?;
 
-    let paint = Paint::default();
+    let mut paint = Paint::default();
+    paint.set_color(match stroke.pen {
+        Pen::Highlighter => Color::from_rgb(255, 255, 0),
+        _ => Color::BLACK,
+    });
+    paint.set_stroke_width(stroke.width);
+    paint.set_style(PaintStyle::Stroke);
 
     let mut path = Path::new();
     for (i, segment) in stroke.segments.iter().enumerate() {
@@ -71,8 +68,6 @@ pub fn render_stroke(stroke: Stroke, canvas: &mut Canvas) -> Result<(), std::io:
             path.line_to((x, y));
         }
     }
-
-    path.close();
 
     canvas.draw_path(&path, &paint);
 
