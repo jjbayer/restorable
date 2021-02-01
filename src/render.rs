b6d0ba88898cc4fcacdc62100c22daa3fd43cbeb
Joris Bayer <joris.bayer@protonmail.com>
2021-02-01 11:25:30 +0100

    Render using skia

--- src/render.rs
+++ src/render.rs
@@ -1,7 +1,7 @@
 use crate::linefile::{Layer, Pen, Stroke};
 use crate::notebook::Notebook;
 use crate::page::Page;
-use skia_safe::Document;
+use skia_safe::{Canvas, Document, Paint, Path};
 use std::io::Write;
 
 // For size, see https://remarkablewiki.com/tech/filesystem
@@ -27,51 +27,54 @@ pub fn render_notebook<Output: Write>(
 }
 
 pub fn render_page(page: Page, document: Document) -> Result<Document, std::io::Error> {
-    let document = document.begin_page((1404, 1874), None);
+    let mut document = document.begin_page((1404, 1874), None);
 
-    // document.render_page(DOCUMENT_WIDTH, DOCUMENT_HEIGHT, |canvas| {
-    //     // Flip y coordinates:
-    //     canvas.concat(Matrix::scale(1.0, -1.0))?;
-    //     canvas.concat(Matrix::translate(0.0, -DOCUMENT_HEIGHT))?;
+    // Flip y coordinates:
+    // canvas.concat(Matrix::scale(1.0, -1.0))?;
+    // canvas.concat(Matrix::translate(0.0, -DOCUMENT_HEIGHT))?;
 
-    //     canvas.set_line_cap_style(CapStyle::Round)?;
-    //     canvas.set_line_join_style(JoinStyle::Round)?;
+    // canvas.set_line_cap_style(CapStyle::Round)?;
+    // canvas.set_line_join_style(JoinStyle::Round)?;
 
-    //     for layer in page.linefile.layers {
-    //         render_layer(layer, canvas)?;
-    //     }
+    for layer in page.linefile.layers {
+        render_layer(layer, document.canvas())?;
+    }
 
-    // })
     Ok(document.end_page())
 }
 
-// pub fn render_layer(layer: Layer, canvas: &mut Canvas) -> Result<(), std::io::Error> {
-//     for stroke in layer.strokes {
-//         render_stroke(stroke, canvas)?;
-//     }
+pub fn render_layer(layer: Layer, canvas: &mut Canvas) -> Result<(), std::io::Error> {
+    for stroke in layer.strokes {
+        render_stroke(stroke, canvas)?;
+    }
 
-//     Ok(())
-// }
+    Ok(())
+}
 
-// pub fn render_stroke(stroke: Stroke, canvas: &mut Canvas) -> Result<(), std::io::Error> {
-//     // TODO: set fill color, pressure, etc.
-//     canvas.set_line_width(stroke.width)?;
-//     canvas.set_stroke_color(match stroke.pen {
-//         Pen::Highlighter => Color::rgb(0, 255, 255),
-//         _ => Color::gray(0),
-//     })?;
+pub fn render_stroke(stroke: Stroke, canvas: &mut Canvas) -> Result<(), std::io::Error> {
+    // TODO: set fill color, pressure, etc.
+    // canvas.set_line_width(stroke.width)?;
+    // canvas.set_stroke_color(match stroke.pen {
+    //     Pen::Highlighter => Color::rgb(0, 255, 255),
+    //     _ => Color::gray(0),
+    // })?;
 
-//     for (i, segment) in stroke.segments.iter().enumerate() {
-//         canvas.set_line_width(STROKE_WIDTH * segment.pressure * stroke.width)?;
-//         let (x, y) = (segment.x, segment.y);
-//         if i == 0 {
-//             canvas.move_to(x, y)?;
-//         } else {
-//             canvas.line_to(x, y)?;
-//         }
-//     }
+    let paint = Paint::default();
 
-//     canvas.stroke()?;
+    let mut path = Path::new();
+    for (i, segment) in stroke.segments.iter().enumerate() {
+        // canvas.set_line_width(STROKE_WIDTH * segment.pressure * stroke.width)?;
+        let (x, y) = (segment.x, segment.y);
+        if i == 0 {
+            path.move_to((x, y));
+        } else {
+            path.line_to((x, y));
+        }
+    }
 
-//     Ok(())
-// }
+    path.close();
+
+    canvas.draw_path(&path, &paint);
+
+    Ok(())
+}
